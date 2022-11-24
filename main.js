0ea630343d4f4650d3a173c63ed3136834cef25b
amoano <113468673+amoano@users.noreply.github.com>
2022-11-24 12:18:03 +0100

    Laddar upp kod till filer

---
+++ main.js
@@ -0,0 +1,88 @@
+function setFormMessage(formElement, type, message) {
+  const messageElement = formElement.querySelector(".form__message");
+
+  messageElement.textContent = message;
+  messageElement.classList.remove(
+    "form__message--success",
+    "form__message--error"
+  );
+  messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+  inputElement.classList.add("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = message;
+}
+
+function clearInputError(inputElement) {
+  inputElement.classList.remove("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
+
+  document
+    .querySelector("#linkCreateAccount")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      loginForm.classList.add("form--hidden");
+      createAccountForm.classList.remove("form--hidden");
+    });
+  document
+    .querySelector("#linkForgottenPassword")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      loginForm.classList.add("form--hidden");
+      forgottenPassword.classList.remove("form--hidden");
+    });
+  document.querySelector("#linkLogin").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
+  });
+  const objPeople = [
+    {
+      username: "janne",
+      password: "test",
+    },
+  ];
+  loginForm.addEventListener("submit", (e) => {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username == "janne" && password == "test") {
+      setFormMessage(loginForm, "error", "Lyckad inloggning!");
+      loginForm.classList.add("form--hidden");
+      info.classList.remove("form--hidden");
+    } else {
+      setFormMessage(loginForm, "error", "Ogiltigt användarnamn/lösenord");
+    }
+
+    e.preventDefault();
+  });
+
+  document.querySelectorAll(".form__input").forEach((inputElement) => {
+    inputElement.addEventListener("blur", (e) => {
+      if (
+        e.target.id === "signupUsername" &&
+        e.target.value.length > 0 &&
+        e.target.value.length < 4
+      ) {
+        setInputError(
+          inputElement,
+          "Användarnamnet måste vara minst 4 tecken långt"
+        );
+      }
+    });
+
+    inputElement.addEventListener("input", (e) => {
+      clearInputError(inputElement);
+    });
+  });
+});
